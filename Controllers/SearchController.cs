27348dd7fb5b5ab93fe635309f7778366356a3a8
Varsha1997 <n00963984@ospreys.unf.edu>
2018-08-20 20:45:05 -0400

    Update SearchController.cs

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +6,14 @@ using System.Web.Mvc;
 using VMS_SummerAssign.Models;
 using PagedList;
 
+
 namespace VMS_SummerAssign.Controllers
 {
     public class SearchController : Controller
     {
         VolunteerDbContext db = new VolunteerDbContext();
-        public ActionResult Search(string option, string search,int?pageNumber)
+
+    public ActionResult Search(string option, string search,int?pageNumber)
         {
             if (option == "FName")
             {
@@ -29,21 +31,43 @@ namespace VMS_SummerAssign.Controllers
 
             //}
         }
-        //[HttpPost]
-       // public ActionResult Create(FormCollection collection)
-        //{
-          //  try
-          //  {
-          //      return RedirectToAction("Index");
-          //  }
-         //   catch
-         //   {
-          //      return View();
-          //  }
-      //  }
-       // public ActionResult Edit(int id)
-       // {
 
-       // }
+        public ActionResult Search2(string option, string search, int? pageNumber)
+        {
+            if (option == "OName")
+            {
+                return View(db.Opportunities.Where(X => X.OName.StartsWith(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 3));
+
+            }
+            else //if (option == "OActivity")
+            {
+                return View(db.Opportunities.Where(X => X.OActivity.StartsWith(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 3));
+
+            }
+            //else
+            //{
+            // return View(db.Volunteers.Where(X => X.Gender.StartsWith(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 3));
+
+            //}
+        }
+
+
+
+        //[HttpPost]
+        // public ActionResult Create(FormCollection collection)
+        //{
+        //  try
+        //  {
+        //      return RedirectToAction("Index");
+        //  }
+        //   catch
+        //   {
+        //      return View();
+        //  }
+        //  }
+        // public ActionResult Edit(int id)
+        // {
+
+        // }
     }
 }
